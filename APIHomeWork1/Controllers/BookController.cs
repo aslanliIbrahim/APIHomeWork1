59cac0a20daf27aef60a44a01ab2bf44aa9e99c9
aslanliIbrahim <ibrahimra@code.edu.az>
2021-10-03 23:30:59 +0400

    Add project files.

---
+++ APIHomeWork1/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using APIHomeWork1.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIHomeWork1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetBook()
+        {
+            var books = _context.Books.ToList();
+
+            if (books == null) return NotFound();
+            
+            return Ok(books);
+        }
+
+        
+        
+    }
+}
---
+++ APIHomeWork1/Migrations/20211003184245_CreateDatabaseForBook.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIHomeWork1.Migrations
+{
+    public partial class CreateDatabaseForBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Writer = table.Column<string>(nullable: false),
+                    SharedDate = table.Column<string>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    ShortAbout = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
